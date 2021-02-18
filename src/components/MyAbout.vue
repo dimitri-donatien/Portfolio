aa0ef02d0259c649caf2900e73ac1221d14ecb4b
dimitri-donatien <donatien.dim@gmail.com>
2021-02-18 07:10:37 +0100

    Update

--- src/components/MyAbout.vue
+++ src/components/MyAbout.vue
@@ -2,7 +2,7 @@
   <section id="#propos" class="flex flex-col items-center justify-center min-h-screen w-full p-4">
     <div class="w-11/12">
       <h2 class="font-bold text-3xl mb-8 md:mb-6 md:text-4xl">À propos<span class="text-blue-600">_</span></h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde exercitationem tempore consectetur atque cupiditate optio mollitia suscipit sed perferendis quam nihil itaque ut adipisci numquam facere, dicta reprehenderit? Minima aspernatur cumque sint fuga praesentium reiciendis doloribus distinctio eligendi placeat delectus, non iure cupiditate explicabo aut necessitatibus? Illum non perspiciatis consectetur dicta esse hic porro velit maiores, cupiditate nam laudantium quos possimus! Saepe debitis delectus libero voluptates, esse inventore doloribus asperiores. Quibusdam excepturi commodi quaerat expedita ratione. Rerum tempora alias animi blanditiis deleniti omnis distinctio labore temporibus enim! Fugit reprehenderit consequuntur culpa numquam vitae iste, totam, aperiam ex odit esse distinctio.</p>
+      <p>Bonjour! Je m’appelle <span class="link">Dimitri Donatien</span>, je suis <span class="link">concepteur développeur d'applications</span>, motivé, curieux et passionné par l'UI / UX et le développement. Ma spécialité est de construire des projets web de A à Z, en commençant par la définition du produit pour répondre au mieux aux besoins, en passant par la conception de l'outil, l'intégration des pages et le développement des différentes fonctionnalités. Sans oublier l'optimisation du référencement naturel et la mise en ligne. Je peux donc vous accompagner sur toutes les étapes de votre projet digital. Pour toute question, n’hésitez pas à me contacter : <a class="link" href="mailto:donatian.dim@gmail.com">donatian.dim@gmail.com</a>.</p>
     </div>
   </section>
 </template>
@@ -16,6 +16,48 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.link {
+  position: relative;
+  text-decoration: none;
+  display: inline-block;
+  color: black;
+  padding: 0 1px;
+  transition: color ease 0.3s;
+  
+}
+
+.link::before, .link::after {
+    content: '';
+    position: absolute;
+    background-color: rgb(248, 113, 113);
+    z-index: -1;
+    height: 5%;
+  }
+
+.link::before {
+    width: 0%;
+    left: 0;
+    bottom: 0;
+    transition: width ease 0.4s;
+  }
+
+.link::after {
+    width: 100%;
+    left: 0;
+    bottom: 0;
+    transition: all ease 0.6s;
+  }
+
+.link:hover::before {
+      width: 100%;
+    }
+
+.link:hover::after {
+      left: 100%;
+      width: 0%;
+      transition: all ease 0.2s;
+    }
 
 
 </style>
--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -2,7 +2,7 @@
   <footer class="background-footer">
     <!-- Copyright Bar -->
     <div class="pt-2">
-        <div class="flex pb-5 px-3 m-auto pt-5 border-t border-gray-500 text-white text-sm flex-col md:flex-row max-w-6xl">
+        <div class="flex pb-5 px-3 m-auto pt-5 border-t border-red-600 text-white text-sm flex-col md:flex-row max-w-6xl">
             <div class="mt-2">
                 © Copyright 2021-2022. All Rights Reserved.
             </div>
@@ -22,7 +22,9 @@
 </script>
 
 <style lang="scss" scoped>
+
   .background-footer {
     background-color: #131313;
   }
+
 </style>
