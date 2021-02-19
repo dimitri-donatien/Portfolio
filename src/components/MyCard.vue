cc49a2c48037027054c7d2893ae24687dafb2ec5
dimitri-donatien <donatien.dim@gmail.com>
2021-02-19 21:28:28 +0100

    Update MyCard and MyPortfolio components.

--- src/components/MyCard.vue
+++ src/components/MyCard.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="card">
-    <div class="card-cover" :style="{ background: 'center / cover no-repeat url(' + image + ')'}">
+    <div class="item">
+      <div class="item-image">
+        <img :src="image" alt="" />
+      </div>
+      <div class="item-text">
+        <div class="item-text-wrapper">
+          <p class="item-text-dek">{{ category }}</p>
+          <h2 class="item-text-title">{{ title }}</h2>
+        </div>
+      </div>
+      <a class="item-link" :href="link" target="_blank" rel="noopener noreferrer"></a>
     </div>
-  </div>
 </template>
 
 <script>
@@ -11,80 +19,111 @@
 export default {
   name: 'MyCard',
   props: {
-    image: String
+    image: String,
+    category: String,
+    title: String,
+    link: String
   }
 }
 </script>
 
 <style lang="scss" scoped>
 
-.card {
+.item {
     position: relative;
-    z-index: 1;
-    width: 100%;
-    padding-bottom: 150%;
-    perspective: 1000px;
-}
-
-.card-cover {
-    position: absolute;
-    z-index: 1;
-    top: 0;
-    left: 0;
+    background-color: rgba(96, 165, 250, .5);
+    overflow: hidden;
+  }
+  .item:after {
+    content: "";
+    display: block;
+    background-color: inherit;
+    opacity: 0.9;
     width: 100%;
     height: 100%;
-    overflow: hidden;
-    background-image: linear-gradient(120deg, #f6d365 0%, #fda085 100%);
-    background-size: cover;
-    perspective-origin: 50% 50%;
-    transform-style: preserve-3d;
-    transform-origin: top center;
-    will-change: transform;
-    transform: skewX(0.001deg);
-    transition: transform 0.35s ease-in-out;
-}
-
-.card-cover::after {
-    display: block;
-    content: "";
     position: absolute;
-    z-index: 100;
+    top: 0;
+    left: 0;
+    transform: scale(2) translateX(-75%) translateY(-75%) translateZ(0) rotate(-30deg);
+    transition: transform 3s cubic-bezier(0.23, 1, 0.32, 1);
+  }
+  .item:hover:after {
+    transform: scale(2) translateX(0%) translateY(0%) translateZ(0) rotate(-30deg);
+  }
+  .item:hover .item-image {
+    transform: scale(1.2) translateZ(0);
+  }
+  .item:hover .item-text {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  
+  .item-image {
+    height: auto;
+    -webkit-backface-visibility: hidden;
+            backface-visibility: hidden;
+    transform: translateZ(0);
+    transition: transform 750ms cubic-bezier(0.23, 1, 0.32, 1);
+  }
+  .item-image::before {
+    content: "";
+    display: block;
+    padding-top: 95%;
+    overflow: hidden;
+  }
+  .item-image img {
+    position: absolute;
     top: 0;
     left: 0;
     width: 100%;
-    height: 120%;
-    background: linear-gradient(226deg, rgba(255, 255, 255, 0.4) 0%, rgba(255, 255, 255, 0.4) 35%, rgba(255, 255, 255, 0.2) 42%, rgba(255, 255, 255, 0) 60%);
-    transform: translateY(-20%);
-    will-change: transform;
-    transition: transform 0.65s cubic-bezier(0.18, 0.9, 0.58, 1);
-}
-
-.card:hover .card-cover {
-    transform: rotateX(7deg) translateY(-6px);
-}
-
-.card:hover .card-cover::after {
-    transform: translateY(0%);
-}
-
-.card::before {
-    display: block;
-    content: "";
+    height: auto;
+    line-height: 0;
+  }
+  
+  .item-text {
     position: absolute;
-    top: 5%;
-    left: 5%;
-    width: 90%;
-    height: 90%;
-    background: rgba(0, 0, 0, 0.5);
-    box-shadow: 0 6px 12px 12px rgba(0, 0, 0, 0.4);
-    will-change: opacity;
-    transform-origin: top center;
-    transform: skewX(0.001deg);
-    transition: transform 0.35s ease-in-out, opacity 0.5s ease-in-out;
-}
-
-.card:hover::before {
-    opacity: 0.6;
-    transform: rotateX(7deg) translateY(-6px) scale(1.05);
-}
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    opacity: 0;
+    text-align: center;
+    z-index: 1;
+    color: #ffffff;
+    transition: opacity 500ms cubic-bezier(0.23, 1, 0.32, 1), transform 500ms cubic-bezier(0.23, 1, 0.32, 1);
+    transition-delay: 300ms;
+    transform: translateY(-20%);
+  }
+  
+  .item-text-wrapper {
+    width: 100%;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  
+  .item-text-title {
+    font-weight: normal;
+    padding: 0 15px;
+    margin: 5px 0 0 0;
+  }
+  
+  .item-text-dek {
+    text-transform: uppercase;
+    font-style: 14px;
+    opacity: 0.7;
+    margin: 0;
+  }
+  
+  .item-link {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 2;
+    line-height: 0;
+    overflow: hidden;
+    text-indent: -9999px;
+  }
 </style>
--- src/components/MyPortfolio.vue
+++ src/components/MyPortfolio.vue
@@ -1,11 +1,12 @@
 <template>
   <section id="portfolio" class="portfolio-content">
-    <h2 class="font-bold text-white text-3xl mb-8 md:mb-0 md:text-4xl">Portfolio<span class="text-red-600">_</span></h2>
-    <div class="content-card">
-      <my-card :image="require('@/assets/img/227944.jpg')"></my-card>
-      <my-card :image="require('@/assets/img/227944.jpg')"></my-card>
-      <my-card :image="require('@/assets/img/227944.jpg')"></my-card>
-      <my-card :image="require('@/assets/img/227944.jpg')"></my-card>
+    <h2 class="font-bold text-white text-3xl mb-8 md:mb-4 md:text-4xl">Portfolio<span class="text-red-600">_</span></h2>
+    <div class="item-grid">
+      <my-card :image="require('@/assets/img/Landing.jpg')" category="Ui/UX" title="Love It"></my-card>
+      <my-card :image="require('@/assets/img/Landing-Blog.jpg')" category="Ui/UX" title="Landing Page Blog"></my-card>
+      <my-card :image="require('@/assets/img/Food-Design.jpg')" category="Ui/UX" title="Food Fitness"></my-card>
+      <my-card :image="require('@/assets/img/Sing-in-anime.jpg')" category="Ui/UX" title="Anime website sing in"></my-card>
+      <my-card :image="require('@/assets/img/Landing-Page-Hosting.jpg')" category="Ui/UX" title="Landing Hosting"></my-card>
     </div>
   </section>
 </template>
@@ -30,11 +31,10 @@ export default {
     background: #121212;
     padding: 1rem;
   }
-  .content-card {
+
+  .item-grid {
     display: grid;
-    grid-template-columns: repeat( auto-fit, minmax(250px, 1fr));
-    place-items: center;
-    min-height: 100vh;
+    grid-template-columns: repeat( auto-fit, minmax(325px, 1fr));
     gap: 1rem;
   }
 </style>
